375155694e31a251b6838cc06cde62f5c14c88bc
Blueberryy <36592509+Blueberryy@users.noreply.github.com>
2020-10-06 17:02:48 +0500

    Create coffeecup.lua (#2)

---
+++ lua/lang/russian/coffeecup.lua
@@ -0,0 +1,8 @@
+L = LANG.GetLanguageTableReference("Русский")
+
+L["coffeecup_granted_reward"] = "Вы нашли чашку кофе и получили {reward} очков!"
+L["coffeecup_hunt_started"] = "Охота за чашкой кофе началась! Количество чашек: {amount}, и каждая из них даст вам {points} очков."
+L["coffeecup_hunt_over"] = "Охота за чашкой кофе окончена."
+L["coffecup_name"] = "Чашка кофе"
+L["coffecup_pickup"] = "Нажмите [{usekey}], чтобы взять чашку кофе."
+L["coffecup_help"] = "Взяв эту чашку кофе, вы получите очки."
