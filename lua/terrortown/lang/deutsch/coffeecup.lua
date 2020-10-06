8bdc5c15e5e3e3bf4c3f60eac9f3dc8684b96372
Tim Goll <tim@timgoll.de>
2020-10-06 14:06:01 +0200

    switched to new folder structure

---
+++ lua/terrortown/lang/deutsch/coffeecup.lua
@@ -0,0 +1,8 @@
+L = LANG.GetLanguageTableReference("deutsch")
+
+L["coffeecup_granted_reward"] = "DU hast die Kaffeetasse gefunden und {reward} Punkt(e) als Belohnung erhalten!"
+L["coffeecup_hunt_started"] = "Die Kaffeetassenjagd hat beginnen! Diese Runde gibt es {amount} Tassen und jede davon gibt {points} Punkt(e)."
+L["coffeecup_hunt_over"] = "Die Kaffeetassenjagd ist vorbei."
+L["coffecup_name"] = "Kaffeetasse"
+L["coffecup_pickup"] = "Drücke [{usekey}] um die Tasse aufzuheben"
+L["coffecup_help"] = "Das Aufheben dieser Kaffeetasse wird dir Punkte geben."
