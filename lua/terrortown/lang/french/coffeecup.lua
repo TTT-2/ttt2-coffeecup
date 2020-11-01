a833d8120ca68f6c373ae3563feb721eca771343
MisterClems <73431903+MisterClems@users.noreply.github.com>
2020-11-01 02:47:34 +0100

    Create coffeecup.lua
    french translation :coffeecup

---
+++ lua/terrortown/lang/french/coffeecup.lua
@@ -0,0 +1,8 @@
+L = LANG.GetLanguageTableReference("french")
+
+L["coffeecup_granted_reward"] = "Vous avez trouvé une tasse de café et vous avez été récompensé de {reward} point(s)!"
+L["coffeecup_hunt_started"] = "La chasse aux tasses de café a commencé! Il y a {amount} tasses et chacune vous accorde {points} point(s)."
+L["coffeecup_hunt_over"] = "La chasse aux tasses de café est terminée."
+L["coffecup_name"] = "Tasse de Café"
+L["coffecup_pickup"] = "Appuyez sur [{usekey}] pour ramasser une tasse de café"
+L["coffecup_help"] = "En ramassant cette tasse de café, vous obtiendrez des points."
